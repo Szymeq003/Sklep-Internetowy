48af2b5aa396b5e5af62a8f68447bc5bab76237a
Szymon Pyrz <szymon.pyrz03@gmail.com>
2026-03-23 18:08:55 +0100

    jwt service

---
+++ auth/src/main/java/org/example/auth/services/UserService.java
@@ -0,0 +1,32 @@
+package org.example.auth.services;
+
+
+import org.example.auth.entity.User;
+import org.example.auth.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+
+    public User saveUser(User user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return  userRepository.saveAndFlush(user);
+    }
+
+    public String generateToken(String username){
+        return jwtService.generateToken(username);
+    }
+
+    public void validateToken(String token){
+        jwtService.validateToken(token);
+    }
+
+}
