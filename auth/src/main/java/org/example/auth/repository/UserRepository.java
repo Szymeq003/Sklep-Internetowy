788f854c21aa6f4b2b4f8b62c13b11d40e79929d
Szymon Pyrz <szymon.pyrz03@gmail.com>
2026-03-30 16:20:40 +0200

    logger apigateway

--- auth/src/main/java/org/example/auth/repository/UserRepository.java
+++ auth/src/main/java/org/example/auth/repository/UserRepository.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findUserByLogin(String login);
-    @Query(nativeQuery = true, value = "SELECT * FROM users where login=?1 and islock=false and isenable=true")
+    @Query(nativeQuery = true, value = "SELECT * FROM users where login=?1 and islock=false and isenabled=true")
     Optional<User> findUserByLoginAndLockAndEnabled(String login);
     Optional<User> findUserByEmail(String email);
     Optional<User> findUserByUuid(String uuid);
--- gatway/src/main/java/org/example/gatway/filter/RouteValidator.java
+++ gatway/src/main/java/org/example/gatway/filter/RouteValidator.java
@@ -9,7 +9,7 @@ import java.util.function.Predicate;
 @Component
 public class RouteValidator {
     public static final List<String> openApiEndpoints = List.of(
-            "auth/register",
+            "/auth/register",
             "/auth/login",
             "/auth/validate",
             "/auth/activate",
